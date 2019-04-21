f3c60877b3294816823972e5d0bc18e2bd6fa591
matthew <matvey343@gmail.com>
2019-04-21 22:24:52 +0500

    release-0.91

--- src/client/Main.java
+++
@@ -1,8 +0,0 @@
-package client;
-
-public class Main {
-    public static void main(String[] args) {
-        Client client = new Client("127.0.0.1", 6666);
-        client.createSocket();
-    }
-}
